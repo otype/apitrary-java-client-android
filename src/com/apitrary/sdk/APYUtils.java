525a77c9049877ea5bba933939acb350570af501
Sebastian Engel <se@apitrary.com>
2012-09-12 18:02:43 +0200

    added getFullApiUrl() to APYUtils class

--- src/com/apitrary/sdk/APYUtils.java
+++ src/com/apitrary/sdk/APYUtils.java
@@ -1,5 +1,7 @@
 package com.apitrary.sdk;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Iterator;
 
 import org.json.JSONException;
@@ -8,6 +10,68 @@ import org.json.JSONObject;
 abstract class APYUtils {
 
     private APYUtils() {
+    }
+
+    /**
+     * Returns the full API URL constructed by the given values.
+     * 
+     * @param apiBaseUrl
+     *            the base API URL consisting of protocol, host and port (e.g.
+     *            "http://HOST:PORT")
+     * @param apiId
+     *            the API ID identifying the API work with
+     * @param apiVersion
+     *            the API's version to work with
+     * @return the full API URL ending with a slash (e.g.
+     *         "http://HOST:PORT/API_ID/API_VERSION/")
+     * @throws MalformedURLException
+     *             if no valid URL could be constructed using the given values
+     */
+    static URL getFullApiUrl(String apiBaseUrl, String apiId, int apiVersion) throws MalformedURLException {
+        StringBuilder urlStringBuilder = new StringBuilder(apiBaseUrl);
+
+        // Check if the apiBaseUrl ends with a slash. If not, append one
+        if (!apiBaseUrl.endsWith("/")) {
+            urlStringBuilder = urlStringBuilder.append("/");
+        }
+
+        // Append the API ID
+        urlStringBuilder = urlStringBuilder.append(apiId);
+
+        // Append the API version and a slash
+        urlStringBuilder = urlStringBuilder.append("/v").append(apiVersion).append("/");
+
+        return new URL(urlStringBuilder.toString());
+    }
+
+    /**
+     * Validates the given entity.
+     * 
+     * @param entity
+     *            the entity to validate
+     * @throws IllegalArgumentException
+     *             when the given entity is null,
+     *             when the entity's name is null or empty
+     */
+    static void validateEntity(APYEntity entity) throws IllegalArgumentException {
+        if (entity == null) {
+            throw new IllegalArgumentException("The entity to validate must not be null.");
+        }
+
+        if (isNullOrEmpty(entity.getName())) {
+            throw new IllegalArgumentException("The name of the entity is null or empty.");
+        }
+    }
+
+    /**
+     * Checks if the given parameter string is null or empty.
+     * 
+     * @param checkedParameter
+     *            the string parameter to check on null or empty
+     * @return true if the parameter is null or empty, false otherwise
+     */
+    static boolean isNullOrEmpty(String checkedParameter) {
+        return checkedParameter == null || checkedParameter.trim().length() == 0;
     }
 
     /**
@@ -81,36 +145,6 @@ abstract class APYUtils {
         }
         
         return apyEntity;
-    }
-
-    /**
-     * Validates the given entity.
-     * 
-     * @param entity
-     *            the entity to validate
-     * @throws IllegalArgumentException
-     *             when the given entity is null,
-     *             when the entity's name is null or empty
-     */
-    static void validateEntity(APYEntity entity) throws IllegalArgumentException {
-        if (entity == null) {
-            throw new IllegalArgumentException("The entity to validate must not be null.");
-        }
-
-        if (isNullOrEmpty(entity.getName())) {
-            throw new IllegalArgumentException("The name of the entity is null or empty.");
-        }
-    }
-
-    /**
-     * Checks if the given parameter string is null or empty.
-     * 
-     * @param checkedParameter
-     *            the string parameter to check on null or empty
-     * @return true if the parameter is null or empty, false otherwise
-     */
-    static boolean isNullOrEmpty(String checkedParameter) {
-        return checkedParameter == null || checkedParameter.trim().length() == 0;
     }
 
 }
